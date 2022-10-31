f52ee37ee09beef7dc6b417737574b12f89fba7b
Mark Whitehead <mjamesw2010@gmail.com>
2022-10-31 22:17:30 +0000

    inserted hard coeded data into forecast.json. Created a locationdetails.js component. Passed down props through app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import '../styles/App.css';
+import PropTypes from 'prop-types';
+import LocationDetails from './LocationDetails';
 
-function App() {
+function App({ location }) {
+  const { city, country } = location;
+  // const city = props.location.city;
+  // const country = props.location.country;
   return (
-    <div>
+    <div className="App">
       <h1>Weather App</h1>
+      <LocationDetails city={city} country={country} />
     </div>
   );
 }
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
