eafb67395dcb73b825a04188d003e4e9a7e6df9f
Mark Whitehead <mjamesw2010@gmail.com>
2022-11-06 12:18:14 +0000

    Testing forecastsummaries

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,34 @@ import PropTypes from 'prop-types';
 import LocationDetails from './LocationDetails';
 import ForecastSummaries from './ForecastSummaries';
 
-function App({ location }) {
-  const { city, country } = location;
+function App(props) {
+  const { forecasts, location } = props;
   // const city = props.location.city;
   // const country = props.location.country;
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <LocationDetails city={city} country={country} />
-      <ForecastSummaries />
+      <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries forecasts={forecasts} />
     </div>
   );
 }
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
   }).isRequired,
 };
-
 export default App;
