415ae72adafd597708e4ae498488acfe63505b31
Mark Whitehead <mjamesw2010@gmail.com>
2022-11-14 22:04:29 +0000

    Setting up on screen error messaging

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,8 @@ function App() {
 
   const [searchText, setSearchText] = useState('');
 
+  const [errorMessage, setErrorMessage] = useState('');
+
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate
   );
@@ -21,27 +23,48 @@ function App() {
     setSelectedDate(date);
   };
   const handleCitySearch = () => {
-    getForecast(searchText, setSelectedDate, setForecasts, setLocation);
+    getForecast(
+      searchText,
+      setSelectedDate,
+      setForecasts,
+      setLocation,
+      setErrorMessage
+    );
   };
 
   useEffect(() => {
-    getForecast('Manchester', setSelectedDate, setForecasts, setLocation);
+    getForecast(
+      'Manchester',
+      setSelectedDate,
+      setForecasts,
+      setLocation,
+      setErrorMessage
+    );
   }, []);
 
   return (
     <div className="weather-app">
       <h1>Weather App</h1>
-      <LocationDetails city={location.city} country={location.country} />
+      <LocationDetails
+        city={location.city}
+        country={location.country}
+        errorMessage={errorMessage}
+      />
       <SearchForm
         searchText={searchText}
         setSearchText={setSearchText}
         onSubmit={handleCitySearch}
       />
-      <ForecastSummaries
-        forecasts={forecasts}
-        onForecastSelect={handleForecastSelect}
-      />
-      {selectedForecast && <ForecastDetails forecast={selectedForecast} />}
+      {!errorMessage && (
+        <>
+          <ForecastSummaries
+            forecasts={forecasts}
+            onForecastSelect={handleForecastSelect}
+          />
+          {selectedForecast && <ForecastDetails forecast={selectedForecast} />}
+        </>
+      )}
+      ;
     </div>
   );
 }
